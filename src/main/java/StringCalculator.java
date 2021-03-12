26ff8d70fb0022f05607d91dac7293a7581dde56
hanoogi <hanoogi@naver.com>
2021-03-12 16:18:41 +0900

    Comma is delimiter, acts as + code 구현(pass)

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,6 +2,11 @@ public class StringCalculator {
     public static int add(String str){
         if(str.isEmpty())
             return 0;
+
+        if(str.contains(",")){
+            String[] numbers = str.split(",");
+            return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+        }
         return Integer.parseInt(str);
     }
 }
