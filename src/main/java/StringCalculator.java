de369448925130fabc2a662dfe323338fe63af1d
hanoogi <hanoogi@naver.com>
2021-03-12 16:11:37 +0900

    single number string #2 code 구현(pass)

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,6 +2,6 @@ public class StringCalculator {
     public static int add(String str){
         if(str.isEmpty())
             return 0;
-        return 1;
+        return Integer.parseInt(str);
     }
 }
